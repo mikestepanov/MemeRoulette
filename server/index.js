897fbbd158349f8d4e3fde9389adeb4e06173cea
mikestepanov <mikestepanov777@gmail.com>
2018-02-21 16:53:45 -0800

    updated socket

--- server/index.js
+++ server/index.js
@@ -6,16 +6,22 @@ const app = express();
 app.use(express.static(__dirname + '/../assets'));
 app.use(bodyParser.json({limit: '50mb'}));
 
-const port = Process.env.PORT || 7991;
+const port = process.env.PORT || 7991;
 const server = app.listen(port, () => {
   console.log(`127.0.0.1:${port}`);
 });
 
-const io = require('socket.io').listen(server);
+const socket = require('socket.io');
+const io = socket.listen(server);
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
   socket.on('connected', (str) => {
     console.log(`a str ${str} recieved`);
   });
 });
+
+module.exports = io;
