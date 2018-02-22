5024bb0c87549056e217363817442ce0e4098966
mikestepanov <mikestepanov777@gmail.com>
2018-02-21 16:04:46 -0800

    added basic server configuration

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const socket = require('socket.io');
 
 const app = express();
+app.use(express.static(__dirname + '/../assets'));
+app.use(bodyParser.json({limit: '50mb'}));
 
 let port = Process.env.PORT || 7991;
+const server = app.listen(port, () => {
+  console.log(`127.0.0.1:${port}`);
+});
