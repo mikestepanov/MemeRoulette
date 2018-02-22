c1c8fa23000361dd453e260bbe042c38454974d6
mikestepanov <mikestepanov777@gmail.com>
2018-02-21 16:34:08 -0800

    added socket.io

--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -1,5 +1,9 @@
 const { ipcRenderer } = require('electron');
+const io = require('socket.io-client');
+const socket = io.connect();
 
 const getMemed = () => {
   ipcRenderer.send('getMemed');
 };
+
+socket.emit('connected', 'omegaPrime');
--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const socket = require('socket.io');
 
 const app = express();
 app.use(express.static(__dirname + '/../assets'));
 app.use(bodyParser.json({limit: '50mb'}));
 
-let port = Process.env.PORT || 7991;
+const port = Process.env.PORT || 7991;
 const server = app.listen(port, () => {
   console.log(`127.0.0.1:${port}`);
+});
+
+const io = require('socket.io').listen(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('connected', (str) => {
+    console.log(`a str ${str} recieved`);
+  });
 });
